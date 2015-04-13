b9a26f6ed1c5e1013fad53da37c7f6d220d71874
shkodaaniram <shkodaaniram@rambler.ru>
2015-04-13 21:13:10 +0300

    initial commit

---
+++ app/src/main/java/com/example/videomanager/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.videomanager;
+
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
+
+import java.io.IOException;
+
+/**
+ * More or less straight out of TextureView's doc.
+ * <p>
+ * TODO: add options for different display sizes, frame rates, camera selection, etc.
+ */
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener {
+    private static final String TAG = MainActivity.TAG;
+
+    private Camera mCamera;
+    private TextureView mTextureView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mTextureView = new TextureView(this);
+        mTextureView.setSurfaceTextureListener(this);
+
+        setContentView(mTextureView);
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        mCamera = Camera.open();
+        Camera.Parameters params = mCamera.getParameters();
+        params.setColorEffect(Camera.Parameters.EFFECT_NEGATIVE);
+        mCamera.setParameters(params);
+        mCamera.startPreview();
+        if (mCamera == null) {
+            // Seeing this on Nexus 7 2012 -- I guess it wants a rear-facing camera, but
+            // there isn't one.  TODO: fix
+            throw new RuntimeException("Default camera not available");
+        }
+
+        try {
+            mCamera.setPreviewTexture(surface);
+            mCamera.startPreview();
+        } catch (IOException ioe) {
+            // Something bad happened
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        // Ignored, Camera does all the work for us
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        mCamera.stopPreview();
+        mCamera.release();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // Invoked every time there's a new Camera preview frame
+        //Log.d(TAG, "updated, ts=" + surface.getTimestamp());
+    }
+}
+
+/*import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}*/
