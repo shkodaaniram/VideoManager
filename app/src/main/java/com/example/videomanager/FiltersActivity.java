16f21c52c2e1d8c42415a1fd29c866f9733add13
shkodaaniram <shkodaaniram@rambler.ru>
2015-05-05 21:43:46 +0300

    work with camera without fixing size of it(attempt 1)

--- app/src/main/java/com/example/videomanager/FiltersActivity.java
+++ app/src/main/java/com/example/videomanager/FiltersActivity.java
@@ -3,15 +3,22 @@ package com.example.videomanager;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.PixelFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
@@ -19,12 +26,24 @@ import android.widget.VideoView;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.view.View.OnClickListener;
 
+import java.io.IOException;
 
-public class FiltersActivity extends ActionBarActivity implements OnClickListener {
+
+public class FiltersActivity extends ActionBarActivity implements OnClickListener, TextureView.SurfaceTextureListener {
 
     ProgressDialog progressDialog;
     VideoView videoView;
     Button downloadVBtn;
+    Button openCameraBtn;
+
+    Camera camera;
+    private TextureView mTextureView;
+    boolean isClicked = false;
+
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    boolean previewing = false;
+    LayoutInflater controlInflater = null;
 
 
     @Override
@@ -35,13 +54,81 @@ public class FiltersActivity extends ActionBarActivity implements OnClickListene
 
         downloadVBtn = (Button)findViewById(R.id.downloadVBtn);
         downloadVBtn.setOnClickListener(this);
+        openCameraBtn = (Button)findViewById(R.id.openCameraBtn);
+        openCameraBtn.setOnClickListener(this);
         videoView = (VideoView)findViewById(R.id.videoView);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        mTextureView = new TextureView(this);
+
+        /*surfaceView = (SurfaceView)findViewById(R.id.cameraPreview);
+
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        controlInflater = LayoutInflater.from(getBaseContext());*/
+
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+
+        camera = Camera.open();
+        Camera.Parameters params = camera.getParameters();
+        params.setColorEffect(Camera.Parameters.EFFECT_NEGATIVE);
+        params.setPreviewSize(videoView.getWidth(), videoView.getHeight());
+        camera.setParameters(params);
+        camera.startPreview();
+        if (camera == null) {
+            // Seeing this on Nexus 7 2012 -- I guess it wants a rear-facing camera, but
+            // there isn't one.  TODO: fix
+            throw new RuntimeException("Default camera not available");
+        }
+
+        try {
+            camera.setPreviewTexture(surface);
+            camera.startPreview();
+        } catch (IOException ioe) {
+            // Something bad happened
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        // Ignored, Camera does all the work for us
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        camera.stopPreview();
+        camera.release();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        // Invoked every time there's a new Camera preview frame
+        //Log.d(TAG, "updated, ts=" + surface.getTimestamp());
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.downloadVBtn:
+                /*if(camera != null) {
+                    camera.stopPreview();
+                    camera.release();
+                    camera = null;
+                    previewing = false;
+                }*/
+                if(previewing){
+                    camera.stopPreview();
+                    previewing = false;
+                }
                 progressDialog = new ProgressDialog(FiltersActivity.this);
                 // Set progressbar title
                 progressDialog.setTitle("Downloading video...");
@@ -77,10 +164,53 @@ public class FiltersActivity extends ActionBarActivity implements OnClickListene
                     }
                 });
                 break;
+            case R.id.openCameraBtn:
+                mTextureView.setSurfaceTextureListener(this);
+                setContentView(mTextureView);
+                break;
             default:
                 break;
         }
     }
+
+   /* @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height) {
+// TODO Auto-generated method stub
+        Camera.Parameters params = camera.getParameters();
+        params.setColorEffect(Camera.Parameters.EFFECT_NEGATIVE);
+        camera.setParameters(params);
+        if(previewing){
+            camera.stopPreview();
+            previewing = false;
+        }
+
+        if (camera != null){
+            try {
+                camera.setPreviewDisplay(surfaceHolder);
+                camera.startPreview();
+                previewing = true;
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+// TODO Auto-generated method stub
+        camera = Camera.open();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+// TODO Auto-generated method stub
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+        previewing = false;
+    }*/
 
 
     @Override
